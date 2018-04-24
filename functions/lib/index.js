0fe96cd8b46c96e2e3be28a15e75c39b00a69a90
Chance Seger <cwseger@gmail.com>
2018-04-24 19:43:36 -0400

    removed console.logs

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,26 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const request = require('request');
 const googleApiKey = 'AIzaSyBtpBBEMpGPGtNdVpgjIljd_oDR4KVWRN0';
-const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
 const autocompleteApiRoot = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
-exports.getTravelTimeData = functions.https.onRequest((req, res) => {
-    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
-    console.log(url);
+const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
+exports.getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
+    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
     request(url, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log('res.send()', body);
             res.set('Access-Control-Allow-Origin', '*');
             res.set('Access-Control-Allow-Methods', 'GET, POST');
             res.status(200).send(body);
         }
     });
 });
-exports.getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
-    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
-    console.log(url);
+exports.getTravelTimeData = functions.https.onRequest((req, res) => {
+    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
     request(url, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log('res.send()', body);
             res.set('Access-Control-Allow-Origin', '*');
             res.set('Access-Control-Allow-Methods', 'GET, POST');
             res.status(200).send(body);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,14 @@ import * as functions from 'firebase-functions';
 const request = require('request');
 
 const googleApiKey = 'AIzaSyBtpBBEMpGPGtNdVpgjIljd_oDR4KVWRN0';
-const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
 const autocompleteApiRoot = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
+const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
 
-export const getTravelTimeData = functions.https.onRequest((req, res) => {
-
-    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
-    console.log(url);
+export const getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
+    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
 
     request(url, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log('res.send()', body);
             res.set('Access-Control-Allow-Origin', '*')
             res.set('Access-Control-Allow-Methods', 'GET, POST')
             res.status(200).send(body);
@@ -20,14 +17,11 @@ export const getTravelTimeData = functions.https.onRequest((req, res) => {
     });
 });
 
-export const getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
-
-    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
-    console.log(url);
+export const getTravelTimeData = functions.https.onRequest((req, res) => {
+    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
 
     request(url, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log('res.send()', body);
             res.set('Access-Control-Allow-Origin', '*')
             res.set('Access-Control-Allow-Methods', 'GET, POST')
             res.status(200).send(body);
