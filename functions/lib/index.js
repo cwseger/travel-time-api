b832c68bd3622b32b70c80ce0c91edc7fdc4f364
Chance Seger <cwseger@gmail.com>
2018-04-24 19:32:55 -0400

    moved googleApiKey to backend and created endpoint for getPlaceAutocompleteData

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2,10 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const request = require('request');
+const googleApiKey = 'AIzaSyBtpBBEMpGPGtNdVpgjIljd_oDR4KVWRN0';
+const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
+const autocompleteApiRoot = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
 exports.getTravelTimeData = functions.https.onRequest((req, res) => {
-    request(req.body, function (error, response, body) {
+    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
+    console.log(url);
+    request(url, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            res.send(body);
+            console.log('res.send()', body);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.set('Access-Control-Allow-Methods', 'GET, POST');
+            res.status(200).send(body);
+        }
+    });
+});
+exports.getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
+    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
+    console.log(url);
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log('res.send()', body);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.set('Access-Control-Allow-Methods', 'GET, POST');
+            res.status(200).send(body);
         }
     });
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,36 @@
 import * as functions from 'firebase-functions';
 const request = require('request');
 
+const googleApiKey = 'AIzaSyBtpBBEMpGPGtNdVpgjIljd_oDR4KVWRN0';
+const distanceMatrixApiRoot = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
+const autocompleteApiRoot = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=';
+
 export const getTravelTimeData = functions.https.onRequest((req, res) => {
-    request(req.body, function(error, response, body) {
+
+    const url = distanceMatrixApiRoot + "/" + req.body + "&key=" + googleApiKey;
+    console.log(url);
+
+    request(url, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            res.send(body);
+            console.log('res.send()', body);
+            res.set('Access-Control-Allow-Origin', '*')
+            res.set('Access-Control-Allow-Methods', 'GET, POST')
+            res.status(200).send(body);
+        }
+    });
+});
+
+export const getPlaceAutocompleteData = functions.https.onRequest((req, res) => {
+
+    const url = autocompleteApiRoot + req.body + "&key=" + googleApiKey;
+    console.log(url);
+
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log('res.send()', body);
+            res.set('Access-Control-Allow-Origin', '*')
+            res.set('Access-Control-Allow-Methods', 'GET, POST')
+            res.status(200).send(body);
         }
     });
 });
